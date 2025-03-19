8f880590d7d89fc3fd39f80ada186d2b6991b71a
tta2 <tta2@cin.ufpe.br>
2025-03-19 13:06:25 -0300

    Menu consertado
    Problema do QUIT solucionado e nome das variáveis mudado

--- main.c
+++ main.c
@@ -7,52 +7,60 @@ int main(void)
     const int screenWidth = 1366;
     const int screenHeight = 768;
 
-    const int recWidth = 1366;
-    const int recHeight = 768;
-    
-    const int textHeight = 100;
+    const int bigTextHeight = 100;
     const int smallTextHeight = 50;
     
     const int smallRecWidth = 400;
     const int smallRecHeight = 80;
     
-    char text[] = "<NOME DO JOGO>";
-    int textWidth = 0;
-    int textLength = strlen(text);
-    for (int i = 0; i < textLength; i++) {(text[i] != ' ')?(textWidth += 70):(textWidth += 30);} textWidth--;
-    int textPosX = (screenWidth - textWidth)/2;
-    int textPosY = (screenHeight - textHeight)/2;
+    char nameText[] = "<NOME DO JOGO>";
+    int nameTextWidth = 0;
+    int nameTextLength = strlen(nameText);
+    for (int i = 0; i < nameTextLength; i++) {(nameText[i] != ' ')?(nameTextWidth += 70):(nameTextWidth += 30);} nameTextWidth--;
+    int nameTextPosX = (screenWidth - nameTextWidth)/2;
+    int nameTextPosY = (screenHeight - bigTextHeight)/2;
     
-    char smallText1[] = "Play";
-    int smallText1Width = strlen(smallText1)*29;
-    int smallText1PosX = (screenWidth - smallText1Width)/2;
-    int smallText1PosY = (screenHeight - smallTextHeight)/2;
+    char playText[] = "Play";
+    int playTextWidth = strlen(playText)*29;
+    int playTextPosX = (screenWidth - playTextWidth)/2;
+    int playTextPosY = (screenHeight - smallTextHeight)/2 - 40;
     
-    char smallText2[] = "Settings";
-    int smallText2Width = strlen(smallText2)*26;
-    int smallText2PosX = (screenWidth - smallText2Width)/2;
-    int smallText2PosY = (screenHeight - smallTextHeight)/2;
+    char settText[] = "Settings";
+    int settTextWidth = strlen(settText)*26;
+    int settTextPosX = (screenWidth - settTextWidth)/2;
+    int settTextPosY = (screenHeight - smallTextHeight)/2 + 60;
     
-    char smallText3[] = "Credits";
-    int smallText3Width = strlen(smallText3)*26;
-    int smallText3PosX = (screenWidth - smallText3Width)/2;
-    int smallText3PosY = (screenHeight - smallTextHeight)/2;
+    char credText[] = "Credits";
+    int credTextWidth = strlen(credText)*26;
+    int credTextPosX = (screenWidth - credTextWidth)/2;
+    int credTextPosY = (screenHeight - smallTextHeight)/2 + 160;
     
-    char smallText4[] = "Quit";
-    int smallText4Width = strlen(smallText4)*24;
-    int smallText4PosX = (screenWidth - smallText4Width)/2;
-    int smallText4PosY = (screenHeight - smallTextHeight)/2;
+    char quitText[] = "Quit";
+    int quitTextWidth = strlen(quitText)*24;
+    int quitTextPosX = (screenWidth - quitTextWidth)/2;
+    int quitTextPosY = (screenHeight - smallTextHeight)/2 + 260;
     
-    Rectangle rec = {(screenWidth - recWidth)/2, (screenHeight - recHeight)/2, recWidth, recHeight};
-    Rectangle smallRec0 = {(screenWidth - smallRecWidth)/2, (screenHeight - smallRecHeight)/2 - 40, smallRecWidth, smallRecHeight};
-    Rectangle smallRec1 = {smallRec0.x, smallRec0.y + 100, smallRec0.width, smallRec0.height};
-    Rectangle smallRec2 = {smallRec0.x, smallRec0.y + 200, smallRec0.width, smallRec0.height};
-    Rectangle smallRec3 = {smallRec0.x, smallRec0.y + 300, smallRec0.width, smallRec0.height};
+    Rectangle smallRec = {(screenWidth - smallRecWidth)/2, (screenHeight - smallRecHeight)/2 - 140, smallRecWidth, smallRecHeight};
+    Rectangle borderRec = {0, 0, screenWidth, screenHeight};
+    Rectangle playRec = {smallRec.x, smallRec.y + 100, smallRec.width, smallRec.height};
+    Rectangle settRec = {smallRec.x, smallRec.y + 200, smallRec.width, smallRec.height};
+    Rectangle credRec = {smallRec.x, smallRec.y + 300, smallRec.width, smallRec.height};
+    Rectangle quitRec = {smallRec.x, smallRec.y + 400, smallRec.width, smallRec.height};
 
-    int clickedSmallRec0 = 0;
-    int clickedSmallRec1 = 0;
-    int clickedSmallRec2 = 0;
-    int clickedSmallRec3 = 0;
+    int clickedPlayRec = 0;
+    int clickedSettRec = 0;
+    int clickedCredRec = 0;
+    int clickedQuitRec = 0;
+    
+    int isDownPlayRec = 0;
+    int isDownSettRec = 0;
+    int isDownCredRec = 0;
+    int isDownQuitRec = 0;
+
+    int playGame = 0;
+    int settGame = 0;
+    int credGame = 0;
+    int quitGame = 0;
 
     InitWindow(screenWidth, screenHeight, "Projeto FPI");
     SetTargetFPS(60);
@@ -62,42 +70,55 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(BLACK);
-            DrawRectangleLinesEx(rec, 5, LIGHTGRAY);
+            DrawRectangleLinesEx(borderRec, 5, LIGHTGRAY);
 
-            DrawText(text, textPosX, textPosY - 240, textHeight, LIGHTGRAY);
-            DrawRectangleRounded(smallRec0, 0.5, 10, LIGHTGRAY);
-            DrawRectangleRounded(smallRec1, 0.5, 10, LIGHTGRAY);
-            DrawRectangleRounded(smallRec2, 0.5, 10, LIGHTGRAY);
-            DrawRectangleRounded(smallRec3, 0.5, 10, LIGHTGRAY);
-            DrawText(smallText1, smallText1PosX, smallText1PosY - 40, smallTextHeight, BLACK);
-            DrawText(smallText2, smallText2PosX, smallText2PosY + 60, smallTextHeight, BLACK);
-            DrawText(smallText3, smallText3PosX, smallText3PosY + 160, smallTextHeight, BLACK);
-            DrawText(smallText4, smallText4PosX, smallText4PosY + 260, smallTextHeight, BLACK);
+            DrawText(nameText, nameTextPosX, nameTextPosY - 240, bigTextHeight, LIGHTGRAY);
+            DrawRectangleRounded(playRec, 0.5, 10, LIGHTGRAY);
+            DrawRectangleRounded(settRec, 0.5, 10, LIGHTGRAY);
+            DrawRectangleRounded(credRec, 0.5, 10, LIGHTGRAY);
+            DrawRectangleRounded(quitRec, 0.5, 10, LIGHTGRAY);
+            DrawText(playText, playTextPosX, playTextPosY, smallTextHeight, BLACK);
+            DrawText(settText, settTextPosX, settTextPosY, smallTextHeight, BLACK);
+            DrawText(credText, credTextPosX, credTextPosY, smallTextHeight, BLACK);
+            DrawText(quitText, quitTextPosX, quitTextPosY, smallTextHeight, BLACK);
             
+            if (playGame);
+            if (settGame);
+            if (credGame);
+            if (quitGame) break;
+
             if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                clickedSmallRec0 = CheckCollisionPointRec(GetMousePosition(), smallRec0);
-                clickedSmallRec1 = CheckCollisionPointRec(GetMousePosition(), smallRec1);
-                clickedSmallRec2 = CheckCollisionPointRec(GetMousePosition(), smallRec2);
-                clickedSmallRec3 = CheckCollisionPointRec(GetMousePosition(), smallRec3);
+                clickedPlayRec = CheckCollisionPointRec(GetMousePosition(), playRec);
+                clickedSettRec = CheckCollisionPointRec(GetMousePosition(), settRec);
+                clickedCredRec = CheckCollisionPointRec(GetMousePosition(), credRec);
+                clickedQuitRec = CheckCollisionPointRec(GetMousePosition(), quitRec);
             }
             
-            if (clickedSmallRec0 && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                DrawRectangleRounded(smallRec0, 0.5, 10, BLACK);
-                DrawText(smallText1, smallText1PosX, smallText1PosY - 40, smallTextHeight, LIGHTGRAY);
+            if (clickedPlayRec && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                DrawRectangleRounded(playRec, 0.5, 10, BLACK);
+                DrawText(playText, playTextPosX, playTextPosY, smallTextHeight, LIGHTGRAY);
+                isDownPlayRec = 1;
             }
-            if (clickedSmallRec1 && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                DrawRectangleRounded(smallRec1, 0.5, 10, BLACK);
-                DrawText(smallText2, smallText2PosX, smallText2PosY + 60, smallTextHeight, LIGHTGRAY);
+            if (clickedSettRec && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                DrawRectangleRounded(settRec, 0.5, 10, BLACK);
+                DrawText(settText, settTextPosX, settTextPosY, smallTextHeight, LIGHTGRAY);
+                isDownSettRec = 1;
             }
-            if (clickedSmallRec2 && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                DrawRectangleRounded(smallRec2, 0.5, 10, BLACK);
-                DrawText(smallText3, smallText3PosX, smallText3PosY + 160, smallTextHeight, LIGHTGRAY);
+            if (clickedCredRec && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                DrawRectangleRounded(credRec, 0.5, 10, BLACK);
+                DrawText(credText, credTextPosX, credTextPosY, smallTextHeight, LIGHTGRAY);
+                isDownCredRec = 1;
             }
-            if (clickedSmallRec3 && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                DrawRectangleRounded(smallRec3, 0.5, 10, BLACK);
-                DrawText(smallText4, smallText4PosX, smallText4PosY + 260, smallTextHeight, LIGHTGRAY);
+            if (clickedQuitRec && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                DrawRectangleRounded(quitRec, 0.5, 10, BLACK);
+                DrawText(quitText, quitTextPosX, quitTextPosY, smallTextHeight, LIGHTGRAY);
+                isDownQuitRec = 1;
             }
 
+            if (isDownPlayRec && IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), playRec)) playGame = 1;
+            if (isDownSettRec && IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), settRec)) settGame = 1;
+            if (isDownCredRec && IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), credRec)) credGame = 1;
+            if (isDownQuitRec && IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), quitRec)) quitGame = 1;
 
         EndDrawing();
     }
