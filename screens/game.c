bce0ea3e5649b51917a53b61f9c42e3b00eccf62
avap2 <alissonvini075@gmail.com>
2025-04-07 23:07:39 -0300

    Update game.c

--- screens/game.c
+++ screens/game.c
@@ -2,7 +2,7 @@
 #include "game.h"
 
 
-void DrawGame(Player* player, EnemyList* enemy_list, ProjectileList* projectile_list, Font myfont) {
+void DrawGame(Player* player, EnemyList* enemy_list, ProjectileList* projectile_list, Font myfont,Power_up_list power_up_list) {
     Texture2D background = LoadTexture("sprites/background.png");
     ClearBackground(BLACK);    
     DrawTexturePro(background,
@@ -18,10 +18,11 @@ void DrawGame(Player* player, EnemyList* enemy_list, ProjectileList* projectile_
     enemyTextures[1] = LoadTexture("sprites/inimigo2.png");
     enemyTextures[2] = LoadTexture("sprites/inimigo3.png");
     DrawEnemies(enemy_list, myfont, enemyTextures);
+    print_power_up_bar(power_up_list); // printa a barra de power ups
         
 }
 
-void UpdateGameplay(Player* player, EnemyList* enemy_list, ProjectileList* projectile_list) {
+void UpdateGameplay(Player* player, EnemyList* enemy_list, ProjectileList* projectile_list, double* time_pass, int *freeze, Power_up_list* power_up_list) {
     int key_pressed = GetCharPressed();
     if (key_pressed >= 32 && key_pressed <= 125) {
         HandleTyping(player, enemy_list, projectile_list, key_pressed);
@@ -30,7 +31,8 @@ void UpdateGameplay(Player* player, EnemyList* enemy_list, ProjectileList* proje
     }
     
     UpdateEnemyWaves(enemy_list);
-    MoveEnemies(enemy_list, player);
+    MoveEnemies(enemy_list, player, freeze, time_pass, power_up_list);
     UpdateProjectile(projectile_list, enemy_list);
     DelayEnemies(enemy_list);
+    activate_power_up(power_up_list, enemy_list, time_pass, freeze);
 }
