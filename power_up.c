b6fa79deb0bf2bd1414c46d2d02022f79cb8a508
avap2 <alissonvini075@gmail.com>
2025-04-07 23:08:11 -0300

    Update power_up.c

--- power_up.c
+++ power_up.c
@@ -33,3 +33,89 @@ void Power_up_delet(EnemyList* enemyList, int number_enemies){ // mata uma quant
     }
 
 }
+
+void Inicializar_power_up_list(Power_up_list* lista){
+    int max = 3;
+    lista->max_power_up = max;
+    lista->number_power_ups = 0;
+    for (int i = 0; i < max; i++){
+        lista->power_up[i].type = 0;
+        lista->power_up[i].rec.x = 0;
+        lista->power_up[i].rec.y = 0;
+        lista->power_up[i].rec.width = 0;
+        lista->power_up[i].rec.height = 0;
+    }
+}
+
+void Adicionar_power_up(int type, Power_up_list* power_up_list){
+    int adicionar = 0;
+
+    if (power_up_list->number_power_ups < power_up_list->max_power_up){ // se ainda tiver espaço para adicionar
+        adicionar = 1;
+        power_up_list->number_power_ups++;
+        Power_up power_up; // define a altura, largura e tipo do power up que vai ser adicionado a lista de power_ups
+        power_up.rec.height = 40;
+        power_up.rec.width = 40;
+        power_up.rec.x = 30;
+        power_up.rec.y = 650; // testar pra ver onde vai ficar
+        power_up.type = type;
+
+        int indice = power_up_list->max_power_up - power_up_list->number_power_ups; // indice onde vai ser colocado o power up
+        power_up_list->power_up[indice] = power_up;
+        
+    }
+
+    if (adicionar){
+        for (int i = 0; i < power_up_list->max_power_up; i++){ // move a posição de todos os power ups na barra
+            if (power_up_list->power_up[i].type != 0){
+                power_up_list->power_up[i].rec.x += 42;
+            }
+        }
+    }
+}
+
+void print_power_up_bar(Power_up_list power_up_list){
+    for (int i = 0; i < power_up_list.max_power_up; i++){
+
+        if (power_up_list.power_up[i].type == 1){ // desenha o power up de congelamento
+            DrawRectangleRec(power_up_list.power_up[i].rec, BLUE);
+        }
+        else if (power_up_list.power_up[i].type == 2){ // desenha o power up de explode os inimigos
+            DrawRectangleRec(power_up_list.power_up[i].rec, YELLOW);
+        }
+
+    }
+}
+
+void activate_power_up(Power_up_list* power_up_list, EnemyList* enemyList, double *time_pass, int* freeze){
+    if (IsKeyPressed(KEY_ENTER)) {
+        Power_up power_up;
+        if(power_up_list->number_power_ups >0){
+            power_up_list->number_power_ups--;
+            for (int i = 0; i < power_up_list->max_power_up; i++){
+                if (power_up_list->power_up[i].type != 0){
+                    power_up.type = power_up_list->power_up[i].type;
+                    power_up_list->power_up[i].rec.height = 0;
+                    power_up_list->power_up[i].rec.width = 0;
+                    power_up_list->power_up[i].type = 0;
+                    break;
+                }
+            }
+
+            if (power_up.type == 1){
+                *freeze = 1; // ativa o congelamento
+                *time_pass = GetTime();
+            }
+
+            else if (power_up.type == 2){
+                Power_up_delet(enemyList, 3);
+            }
+
+            for (int i = 0; i < power_up_list->max_power_up; i++){ // move a posição de todos os power ups na barra
+                if (power_up_list->power_up[i].type != 0){
+                    power_up_list->power_up[i].rec.x -= 42;
+                }
+            }
+        }
+    }
+}
